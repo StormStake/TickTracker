dcfb35ad7b44f54218c952766b74e5141d4e8717
StormStake <59213963+StormStake@users.noreply.github.com>
2021-01-16 00:05:25 -0600

    Fix

--- main.py
+++ main.py
@@ -1,103 +1,179 @@
+from kivy.uix.boxlayout import BoxLayout
+
+from kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy_garden.graph import Graph, SmoothLinePlot
+from kivy.core.window import Window
+from kivy.lang import Builder
+import reqhelp
 import requests
 import json
-import time
 import threading
+import time
+Builder.load_string("""
+<MyImage>:
+    bcolor: 0, 0, 0, 1
+    canvas.before:
+        Color:
+            rgba: root.bcolor
+        Rectangle:
+            size: self.size
+            pos: self.pos
+""")
 
-class helper():
-    def __init__(self):
-        self.publicdata = []
-        self.cryptos = []
-        self.data = {}
-        self.listoftargets = []
-        for dicto in json.loads(requests.get('https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=bvtss4748v6pijnevmqg').text):
-            self.cryptos.append(dicto['symbol'])
-        self.dt = 0
+Builder.load_string("""
+<MyBox>:
+    bcolor: 0, 0, 0, 1
+    canvas.before:
+        Color:
+            rgba: root.bcolor
+        Rectangle:
+            size: self.size
+            pos: self.pos
+""")
 
+class MyBox(BoxLayout):
+    pass
+class MyImage(Image):
+    pass
 
-    def addtotargets(self, ticker):
-        if ticker not in self.listoftargets:
-            self.listoftargets.append(ticker)
-        else:
-            return(False)
+class Myapp(App):
 
-
-    def threadingtarget(self):
-        def thing():
-            self.obatinresults(self.listoftargets, 'bvtss4748v6pijnevmqg')
-        threading.Timer(1/30, thing).start()
-
-
-    def getdata(self, ticker):
-        try:
-            return self.data[ticker]
-        except:
-            return 0
-
-    def start(self):
-
-        x = threading.Thread(target=self.threadingtarget)
-        x.start()
+    #App Entry Point
+    def build(self):
 
 
+        #for fullscreen use
+        #Window.fullscreen = 'auto'
+
+        #for window use
+        #Window.borderless = '0'
+        Window.size = (1280,720)
+        #Window.clearcolor = (0.1,0.1,0.1,1)
+        #layout setup
+        tickers = ['IBM','AAPL','BINANCE:BTCUSDT']
+
+        pages = []
+        pageslay = PageLayout(border="20")
+
+        for i in range(len(tickers)):
+            pages.append(GraphPage(tickers[i]))
+        for i in range(len(pages)):
+            pageslay.add_widget(pages[i].getWidget())
 
 
-    def obatinresults(self, tickers, key):
+
+        requer = reqhelp.helper()
+
 
         for ticker in tickers:
-            if ticker in self.cryptos:
+            requer.addtotargets(ticker)
 
-                self.data[ticker] = self.getCrytpoPlot(ticker, key)
-            else:
-                self.data[ticker] = self.getStockPlot(ticker, key)
+        requer.start()
 
+        def update(*rap):
+            for page in pages:
+                data = requer.getdata(page.getTicker())
+                
+                if data != 0:
+                    page.getchart().update(data)
+                    page.Currentpricelabel.text = f'Current Quote:\n {str(round(page.getchart().CurrentPrice,2))}'
 
-
-
-    def getStockPlot(self, ticker ,key):
-
-
-        relist = []
-
-        fom = int(time.time() - 31560000)
-        to = int(time.time())
-
-        urlparams = {
-            'symbol':str(ticker),
-            'resolution':'D',
-            'from':f'{fom}',
-            'to':f'{to}',
-            'token': key
-        }
-
-        data = json.loads(requests.get('https://finnhub.io/api/v1/stock/candle', params=urlparams).text)
-        timestamps = data['t']
-        prices = data['c']
-        ymax = max(data['c'])
-        ymin = min(data['c'])
-        for times, value in zip(range(len(prices)) , prices):
-            relist.append((times,value))
-        return {'plot':relist, 'ymax':ymax, 'CurrentPrice': relist[-1][1],'ymin': ymin, 'ticker':ticker}
+        Clock.schedule_interval(update, 1/30)
+        return pageslay
 
 
 
-    def getCrytpoPlot(self, ticker, key):
-        returnlist = []
-        fom = int(time.time() - 31560000)
-        to = int(time.time())
-        urlparams = {
-            'symbol':str(ticker),
-            'resolution':'D',
-            'from':f'{fom}',
-            'to':f'{to}',
-            'token': key
-        }
-        data = json.loads(requests.get('https://finnhub.io/api/v1/crypto/candle', params=urlparams).text)
-        timestamps = data['t']
-        prices = data['c']
-        ymax = max(data['c'])
-        ymin = min(data['c'])
+class GraphPage():
+    def __init__(self, ticker):
 
-        for times, value in zip(range(len(prices)) , prices):
-            returnlist.append((times,value))
-        return {'plot':returnlist, 'ymax':ymax, 'CurrentPrice': returnlist[-1][1],'ymin': ymin, 'ticker':ticker}
+        self.chart = linechart(ticker)
+        self.ticker = ticker
+        self.Page = MyBox(orientation="vertical")
+        self.Currentpricelabel = Label(text=f'Current Quote:\n {str(round(self.chart.CurrentPrice, 2))}', font_size='35sp')
+        #Upper Layout
+        upperLayout = MyBox(orientation="horizontal")
+        upperLayout.add_widget(self.Currentpricelabel)
 
+
+        try:
+            upperLayout.add_widget(MyImage(source=f"{''.join(ticker.split(':'))}.png"))
+        except:
+            upperLayout.add_widget(MyImage(source=f'NA.png'))
+        #Lower Layout
+        self.Page.add_widget(upperLayout)
+        self.Page.add_widget(self.chart.Get_graph())
+    def getWidget(self):
+        return self.Page
+
+    def getchart(self):
+        return self.chart
+
+    def getTicker(self):
+        return self.ticker
+
+class linechart():
+    def __init__(self, ticker):
+        self.CurrentPrice = 100
+        self.ticker = ticker
+        self.cryptos = []
+        self.yticks = 100
+
+
+
+        self.xticks = 10
+        self.xmax = 50
+        self.chart = Graph(
+            xlabel='Time',
+            ylabel='Price',
+            x_ticks_minor=0,
+            x_ticks_major=self.xticks,
+            y_ticks_major=self.yticks,
+            y_grid_label=True,
+            x_grid_label=True,
+            padding=5,
+            x_grid=True,
+            y_grid=True,
+            xmin=0,
+            xmax=self.xmax,
+            ymin=0,
+            )
+
+
+        self.plot = SmoothLinePlot(color=[1, 1, 1, 1])
+        self.chart.add_plot(self.plot)
+
+
+
+
+    def update(self, data):
+
+
+        #sizing graph adding upper buffer
+        self.chart.ymax = round((data['ymax']+data['ymax']/4)/10)*10
+        self.chart.ymin = round((data['ymin']-data['ymin']/4)/10)*10
+        #sizing xmax
+        self.xmax = len(data['plot'])
+
+        self.plots = data['plot']
+        self.CurrentPrice = data['CurrentPrice']
+
+        
+        self.yticks = (self.chart.ymax - self.chart.ymin)/4
+        self.chart.y_ticks_major = self.yticks
+        self.plot.points = self.plots
+        self.xmax = len(self.plot.points)-1
+        self.chart.xmax = self.xmax
+
+
+
+
+    def Get_graph(self):
+        return self.chart
+
+
+Myapp().run()
