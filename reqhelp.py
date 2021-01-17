e80957d7ab441a9b3dec089782b74fad4e85c946
StormStake <59213963+StormStake@users.noreply.github.com>
2021-01-16 21:26:31 -0600

    PI

--- reqhelp.py
+++ reqhelp.py
@@ -101,7 +101,7 @@ class helper():
             return {'plot':plot, 'ymax':ymax, 'CurrentPrice': plot[-1][1],'ymin': ymin, 'ticker':ticker}
 
         except KeyError:
-            return {'CurrentPrice': 0}
+            return {'Fail': True}
 
     #level one api
     def getCrytpoPlot(self, ticker, key):
